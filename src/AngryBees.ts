1022928a4ee89947000f5541b84442205f43e10b
Evan Suma Rosenberg <suma@umn.edu>
2023-02-09 16:34:10 -0600

    class commit

--- src/AngryBees.ts
+++ src/AngryBees.ts
@@ -8,14 +8,35 @@ import * as gfx from 'gophergfx'
 
 export class AngryBees extends gfx.GfxApp
 {
+    private ground: gfx.BoxMesh;
+
     constructor()
     {
         super();
+
+        this.ground = new gfx.BoxMesh();
     }
 
     createScene(): void 
     {
+        // Setup the camera projection matrix and position.
+        // We will learn more about camera models later in this course.
+        this.camera.setPerspectiveCamera(60, 1920/1080, 0.1, 100);
+        this.camera.position.set(0, 1.5, 0);
 
+        // Create an ambient light that illuminates everything in the scene
+        const ambientLight = new gfx.AmbientLight(new gfx.Color(0.4, 0.4, 0.4));
+
+        // Create a directional light that is infinitely far away (sunlight)
+        const directionalLight = new gfx.DirectionalLight(new gfx.Color(0.6, 0.6, 0.6));
+        directionalLight.position.set(1, 2, 1);
+
+        this.ground.position.set(0, -0.5, 0);
+        this.ground.scale.set(50, 1, 50);
+
+        this.scene.add(ambientLight);
+        this.scene.add(directionalLight);
+        this.scene.add(this.ground);
     }
 
     update(deltaTime: number): void 
